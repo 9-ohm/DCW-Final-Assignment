df20ae4bd0ac8b49caef6a3fd7f4f9411849684b
o_Ohm <5910110262@email.psu.ac.th>
2022-03-13 11:37:03 +0700

    try another commiter

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+app.use(bodyParser.urlencoded({extended: false}))
 app.use('/static', express.static('public'))
 
 app.get('/', (req, res) => {
@@ -11,6 +13,12 @@ app.get('/', (req, res) => {
 app.get('/addg', (req, res) => {
     let a = parseInt(req.query.a)
     let b = parseInt(req.query.b)
+    res.send(' ' + (a + b))
+  })
+
+  app.post('/addg', (req, res) => {
+    let a = parseInt(req.body.a)
+    let b = parseInt(req.body.b)
     res.send(' ' + (a + b))
   })
 
