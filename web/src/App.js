4468671be73f5e47e667a8e028f25462d616b057
o_Ohm <5910110262@email.psu.ac.th>
2022-03-19 23:00:49 +0700

    facebook access token

--- web/src/App.js
+++ web/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+
+import FacebookLogin from 'react-facebook-login';
+ 
+const responseFacebook = (response) => {
+  console.log(response);
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <FacebookLogin
+        appId="1772502426429723"
+        autoLoad={true}
+        fields="name,email,picture"
+        callback={responseFacebook} />
     </div>
   );
 }
