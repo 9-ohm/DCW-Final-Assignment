dacd7e94e7b2c5d6c549f13898f5956fe97b18de
o_Ohm <5910110262@email.psu.ac.th>
2022-03-19 23:38:07 +0700

    verify token

--- server/src/main.js
+++ server/src/main.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const axios = require('axios')
 const app = express()
 const port = 3000
 
@@ -6,6 +8,18 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.post('/api/login', bodyParser.json(), async (req, res)=>{
+    let token = req.body.token
+    let result = await axios.get('https://graph.facebook.com/me',{
+        params: {
+            fields: 'id,name,email',
+            access_token: token
+        }
+    })
+    console.log(result.data)
+    res.send({ok: 1})
+
+})
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
 })
