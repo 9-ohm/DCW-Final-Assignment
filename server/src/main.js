6d3540f5ff9e79ec6e830f749e1e2bfb77cd9244
o_Ohm <5910110262@email.psu.ac.th>
2022-03-21 23:37:04 +0700

    issue token

--- server/src/main.js
+++ server/src/main.js
@@ -5,9 +5,12 @@ const axios = require('axios')
 const app = express()
 const port = 3000
 
+const TOKEN_SECRET = '62378da13d281d6e1cf0c59440af4e660d079107487237677ec570c11c2415310e1d9fde642e31d0aaaa71ff495a14936d851b0c358abd07c5f9d0703ebea5f4'
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
 
 app.use(cors())
 
@@ -19,10 +22,17 @@ app.post('/api/login', bodyParser.json(), async (req, res)=>{
             access_token: token
         }
     })
-    console.log(result.data)
-    res.send({ok: 1})
-
+    if(!result.data.id){
+      res.sendStatus(403)
+      return
+    }
+    let data = {
+      username: result.data.email
+    }
+    let access_token = jwt.sign(data, TOKEN_SECRET, {expiresIn: '1800s'})
+    res.send({access_token, username: data.username})
 })
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
 })
--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,15 @@
 
 import FacebookLogin from 'react-facebook-login';
+import axios from 'axios';
  
-const responseFacebook = (response) => {
-  console.log(response);
+const responseFacebook = async (response) => {
+  if(response.accessToken){
+    console.log('log in with accesstoken=' + response.accessToken)
+    let result = await axios.post('http://localhost:8000/api/login',{
+      token: response.accessToken
+    })
+    console.log(response);
+  }
 }
 
 function App() {
