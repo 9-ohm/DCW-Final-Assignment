2368b932b10e3b6a91c66f462771d98982e669e2
o_Ohm <5910110262@email.psu.ac.th>
2022-03-24 22:30:56 +0700

    session storage

--- web/src/App.js
+++ web/src/App.js
@@ -1,15 +1,30 @@
 
 import FacebookLogin from 'react-facebook-login';
 import axios from 'axios';
- 
+
+axios.interceptors.request.use(function(config){
+  const token = sessionStorage.getItem('access_token')
+  if(token)
+    config.headers['Authorization'] = `Bearer ${token}`
+  return config
+}, function(err){
+  return Promise.reject(err)
+})
+
 const responseFacebook = async (response) => {
   if(response.accessToken){
     console.log('log in with accesstoken=' + response.accessToken)
-    let result = await axios.post('http://localhost:8000/api/login',{
+    let result = await axios.post('http://localhost:8080/api/login',{
       token: response.accessToken
     })
     console.log(response);
+    sessionStorage.setItem('access_token', result.data.access_token)
   }
+}
+
+const callInfoAPI = async () =>{
+  let result = await axios.get('http:localhost:8080/api/info')
+  console.log(result.data)
 }
 
 function App() {
@@ -20,6 +35,7 @@ function App() {
         autoLoad={true}
         fields="name,email,picture"
         callback={responseFacebook} />
+        <button onClick={callInfoAPI}>Click me</button>
     </div>
   );
 }
